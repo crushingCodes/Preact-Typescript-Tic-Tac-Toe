e287d11d9a0b23f0e19096fd515b0a074996f9dd
Gavin <gavinjboyd@gmail.com>
2020-10-01 23:05:20 +0900

    added class to board

--- src/components/board/index.tsx
+++ src/components/board/index.tsx
@@ -1,11 +1,33 @@
-import { FunctionalComponent, h } from "preact";
+import { Component, h } from "preact";
+interface BoardComponentState {
+    time: number;
+}
 
-const Board: FunctionalComponent = () => {
-    return (
-        <body>
-            <h1>Board</h1>
-        </body>
-    );
-};
+class Board extends Component<any, BoardComponentState> {
+    timer: any;
+    constructor() {
+        super();
+        this.state = { time: Date.now() };
+    }
+
+    // Lifecycle: Called whenever our component is created
+    componentDidMount() {
+        // update time every second
+        this.timer = setInterval(() => {
+            this.setState({ time: Date.now() });
+        }, 1000);
+    }
+
+    // Lifecycle: Called just before our component will be destroyed
+    componentWillUnmount() {
+        // stop when not renderable
+        clearInterval(this.timer);
+    }
+
+    render() {
+        const time = new Date(this.state.time).toLocaleTimeString();
+        return <h2>{time}</h2>;
+    }
+}
 
 export default Board;
