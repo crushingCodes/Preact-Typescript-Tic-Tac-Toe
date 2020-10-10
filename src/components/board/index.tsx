2d77c6b32ca8c49551aea9172a1b0e5b21b02d23
Gavin <gavinjboyd@gmail.com>
2020-10-10 14:49:06 +0900

    added numbered squares

--- src/components/board/index.tsx
+++ src/components/board/index.tsx
@@ -1,8 +1,25 @@
-import { Component, h } from "preact";
+import { Component, FunctionalComponent, h } from "preact";
+import * as style from "./style.css";
+import { Link } from "preact-router/match";
 interface BoardComponentState {
     time: number;
 }
+interface SqaureComponentState {
+    value: number;
+}
 
+// const Square: FunctionalComponent = ({ value }) => {
+//     return <div className={style.square}>{value}</div>;
+// };
+
+class Square extends Component<any, SqaureComponentState> {
+    constructor() {
+        super();
+    }
+    render(): preact.ComponentChild {
+        return <button className={style.square}>{this.props.value}</button>;
+    }
+}
 class Board extends Component<any, BoardComponentState> {
     timer: any;
     constructor() {
@@ -26,7 +43,26 @@ class Board extends Component<any, BoardComponentState> {
 
     render() {
         const time = new Date(this.state.time).toLocaleTimeString();
-        return <h2>{time}</h2>;
+        return (
+            <div>
+                <div className={style.status}>{status}</div>
+                <div className={style["board-row"]}>
+                    <Square value={0}></Square>
+                    <Square value={1}></Square>
+                    <Square value={2}></Square>
+                </div>
+                <div className={style["board-row"]}>
+                    <Square value={3}></Square>
+                    <Square value={4}></Square>
+                    <Square value={5}></Square>
+                </div>
+                <div className={style["board-row"]}>
+                    <Square value={6}></Square>
+                    <Square value={7}></Square>
+                    <Square value={8}></Square>
+                </div>
+            </div>
+        );
     }
 }
 
